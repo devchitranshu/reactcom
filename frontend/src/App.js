d2a5fd5e07a6e9dbc3009897f8eac069b3b5b8aa
Cezary Olejarczyk <colejarczyk@divante.pl>
2017-03-05 22:19:31 +0100

    NOTASK: Added pagination for products.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,55 @@
 import React, {Component} from 'react';
+import {
+    BrowserRouter as Router,
+    Route,
+    Link
+} from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
     render() {
         return (
-            <div className="App">
-                <div className="App-header">
-                    <img src={logo} className="App-logo" alt="logo"/>
-                    <h2>Welcome to React Ecommerce App</h2>
-                </div>
-                <div>
-                    <div className="categories">
-                        <Category />
+            <Router>
+                <div className="App">
+                    <div className="App-header">
+                        <img src={logo} className="App-logo" alt="logo"/>
+                        <h2>Welcome to React Ecommerce App</h2>
                     </div>
-                    <div className="products">
-                        <Product />
+                    <div>
+                        <div className="categories">
+                            <Category />
+                        </div>
+                        <div className="products">
+                            <Route exact={true} path="/" component={Product} />
+                            <Route path="/page/:pageNumber" component={Product} />
+                        </div>
                     </div>
                 </div>
-            </div>
+            </Router>
         );
     }
+}
+
+function fetchProducts(parent, page) {
+    let pageNumber = 1;
+    if (typeof page !== 'undefined') {
+        pageNumber = page;
+    }
+
+    let url = 'http://m2.localhost:8061/rest/V1/products?searchCriteria[pageSize]=20&searchCriteria[currentPage]=' + pageNumber;
+    fetch(url)
+        .then(result => {
+            return result.json()
+        })
+        .then(json => {
+            parent.setState({products: json.items});
+            parent.setState({total_count: json.total_count});
+            parent.setState({pageNumber: pageNumber});
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
 }
 
 class Product extends Component {
@@ -28,27 +57,25 @@ class Product extends Component {
         super(props);
         this.state = {
             products: [],
-            total_count: 0
+            total_count: 0,
+            pageNumber: 1
         };
     }
 
     componentDidMount() {
-        fetch(`http://m2.localhost:8061/rest/V1/products?searchCriteria[pageSize]=20`)
-            .then(result => {
-                return result.json()
-            })
-            .then(json => {
-                this.setState({products: json.items});
-                this.setState({total_count: json.total_count});
-            })
-            .catch(function (error) {
-                console.log(error);
-            });
+        fetchProducts(this, this.props.match.params.pageNumber);
+    }
+
+    componentDidUpdate(prevProps) {
+        if (this.props.match.params.pageNumber !== prevProps.match.params.pageNumber) {
+            fetchProducts(this, this.props.match.params.pageNumber);
+        }
     }
 
     render() {
         return (
             <div>
+                <Pagination pageNumber={this.state.pageNumber} />
                 { this.state.products.map(item => {
                     let productKey = item.id + "product";
                     let imageKey;
@@ -73,6 +100,21 @@ class Product extends Component {
         );
     }
 }
+
+const Pagination = ({pageNumber}) => {
+    let nextPage = parseInt(pageNumber) + 1.;
+    let prevPage = parseInt(pageNumber) - 1;
+    return(
+        <p>
+            <span className="page">
+                <Link to={`/page/${prevPage}`}>Prev</Link>
+            </span>
+            <span className="page">
+                <Link to={`/page/${nextPage}`}>Next</Link>
+            </span>
+        </p>
+    )
+};
 
 class Category extends Component {
     constructor(props) {
@@ -100,7 +142,7 @@ class Category extends Component {
             <div>
                 { this.state.categories.map(item => {
                     return (
-                        <div className="category">
+                        <div className="category" key={item.id}>
                             {item.name}
                         </div>
                     )
